5f20d4dc519bff40a6abad6755c65c20936a5ae5
Larissa Araujo <larissa.jvsa@gmail.com>
2022-02-13 18:52:30 -0300

    requisito 7

--- src/components/TableWallet.js
+++ src/components/TableWallet.js
@@ -1,23 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class TableWallet extends Component {
   render() {
+    const { expenses } = this.props;
+    // para evitar o warning no console, utilizei a tag <thead> e <tbody>
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((eachExpense) => {
+            const {
+              id,
+              value,
+              description,
+              currency,
+              method,
+              tag,
+              exchangeRates,
+            } = eachExpense;
+            return (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name.split('/')[0]}</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>
+                  {Number(value * exchangeRates[currency].ask).toFixed(2)}
+                </td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default TableWallet;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+TableWallet.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(TableWallet);
